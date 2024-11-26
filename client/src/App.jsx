244a8aef2f0c64cc8326f03d693ee214617a1722
Aakash Pal <akashpal1482@gmail.com>
2024-11-26 09:32:12 +0530

    frontend setup initialized of LMS project

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='font-sans text-6xl text-red-500 bg-green-500'>
+        I am going to start forntend of LMS
+      </div>
+    </>
+  )
+}
+
+export default App
