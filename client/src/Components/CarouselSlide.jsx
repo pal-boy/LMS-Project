c0f96b6d0f7d7f2a3ad6544e226ec0921442c085
Aakash Pal <akashpal1482@gmail.com>
2024-11-27 21:22:17 +0530

    code cleaned up for about us page

---
+++ client/src/Components/CarouselSlide.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const CarouselSlide = ({image , title, description, slideNum, totalSlides }) => {
+  return (
+    <div id={`slide${slideNum}`} className="carousel-item relative w-full">
+        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+            <img
+                src={image}
+                className="w-40 rounded-full border-2 border-gray-400" />
+            <p className='text-xl text-gray-200'>
+                {description}
+            </p>
+            <h3 className='text-2xl font-semibold'>
+                {title}
+            </h3>
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href={`#slide${(slideNum == 1? totalSlides:slideNum-1)}`} className="btn btn-circle">❮</a>
+                <a href={`#slide${(slideNum + 1)% totalSlides+1}`} className="btn btn-circle">❯</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CarouselSlide
--- client/src/Pages/AboutUs.jsx
+++ client/src/Pages/AboutUs.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import HomeLayout from '../Layouts/HomeLayout.jsx'
 import aboutMainImage from '../Assests/aboutMainImage.png'
-import apj from '../Assests/apj.png'
-import billGates from '../Assests/billGates.png'
-import einstein from '../Assests/einstein.png'
-import nelsonMandela from '../Assests/nelsonMandela.png'
-import steveJobs from '../Assests/steveJobs.png'
+import { celebrities } from '../Constants/FamousPeople.js'
+import CarouselSlide from '../Components/CarouselSlide.jsx'
 
 const AboutUs = () => {
   return (
@@ -33,92 +30,16 @@ const AboutUs = () => {
                 </div>
             </div>
 
-            <div className="carousel w-1/2 my-16 m-auto">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img
-                        src={apj}
-                        className="w-40 rounded-full border-2 border-gray-400" />
-                        <p className='text-xl text-gray-200'>
-                        Learning gives creativity, creativity leads to thinking, thinking provides knowledge, and knowledge makes you great.
-                        </p>
-                        <h3 className='text-2xl font-semibold'>
-                            Dr. APJ Abdul Kalam
-                        </h3>
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide5" className="btn btn-circle">❮</a>
-                            <a href="#slide2" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img
-                        src={billGates}
-                        className="w-40 rounded-full border-2 border-gray-400" />
-                        <p className='text-xl text-gray-200'>
-                        Success is a lousy teacher. It seduces smart people into thinking they can't lose.
-                        </p>
-                        <h3 className='text-2xl font-semibold'>
-                            Mr. Bill Gates
-                        </h3>
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide1" className="btn btn-circle">❮</a>
-                            <a href="#slide3" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img
-                        src={einstein}
-                        className="w-40 rounded-full border-2 border-gray-400" />
-                        <p className='text-xl text-gray-200'>
-                        Education is not the learning of facts, but the training of the mind to think.
-                        </p>
-                        <h3 className='text-2xl font-semibold'>
-                            Albert Einstein
-                        </h3>
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide2" className="btn btn-circle">❮</a>
-                            <a href="#slide4" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img
-                        src={nelsonMandela}
-                        className="w-40 rounded-full border-2 border-gray-400" />
-                        <p className='text-xl text-gray-200'>
-                            Education is the most powerful weapon which you can use to change the world.
-                        </p>
-                        <h3 className='text-2xl font-semibold'>
-                            Nelson Mandela
-                        </h3>
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide3" className="btn btn-circle">❮</a>
-                            <a href="#slide5" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                </div>
-                <div id="slide5" className="carousel-item relative w-full">
-                    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                        <img
-                        src={steveJobs}
-                        className="w-40 rounded-full border-2 border-gray-400" />
-                        <p className='text-xl text-gray-200'>
-                        Education is the passport to the future, for tomorrow belongs to those who prepare for it today.
-                        </p>
-                        <h3 className='text-2xl font-semibold'>
-                            Steve Jobs
-                        </h3>
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide4" className="btn btn-circle">❮</a>
-                            <a href="#slide1" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                </div>
+            <div className="carousel w-1/2 m-auto my-16">
+                    {celebrities && celebrities.map(celebrity => (<CarouselSlide 
+                                                                    {...celebrity} 
+                                                                    key={celebrity.slideNum} 
+                                                                    totalSlides={celebrities.length}
+                                                                    
+                                                                />)
+                                                    )
+                    }
+                    
             </div>
         </div>
     </HomeLayout>
