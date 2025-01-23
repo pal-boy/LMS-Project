b6ca1fdc38e59c76ce9400007ce978572326aa7a
Aakash Pal <akashpal1482@gmail.com>
2025-01-23 20:13:15 +0530

    add course slice and integrate course data fetching with async thunk

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from './slices/AuthSlice.js'
+import courseSliceReducer from './slices/CourseSlice.js'
 
 const store = configureStore({
     reducer: {
-        auth : authSliceReducer
+        auth : authSliceReducer,
+        course : courseSliceReducer
     },
     devTools: true
 });
