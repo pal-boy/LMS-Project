6c4662b93b913d3310f382ebe97ba5e87cace0ab
Aakash Pal <akashpal1482@gmail.com>
2025-02-09 20:25:18 +0530

    about page responsiveness added

--- client/src/Pages/AboutUs.jsx
+++ client/src/Pages/AboutUs.jsx
@@ -7,18 +7,20 @@ import CarouselSlide from '../Components/CarouselSlide.jsx'
 const AboutUs = () => {
   return (
     <HomeLayout>
-        <div className="pl-20 pt-5 flex flex-col text-white">
-            <div className="flex items-center gap-5 mx-10">
-                <section className="w-1/2 space-y-10">
-                    <h1 className='text-5xl text-yellow-500 font-semibold'>
+        <div className="pl-10 sm:pl-20 pt-5 flex flex-col text-white">
+            <div className="sm:flex sm:items-center gap-5 sm:mx-10">
+                <section 
+                    style={{'--aboutimage-url': `url(${aboutMainImage})`}} 
+                    className={`mt-20 bg-[image:var(--aboutimage-url)] bg-contain bg-center bg-no-repeat sm:w-1/2 sm:space-y-10 sm:bg-none`}>
+                    <h1 className='text-3xl sm:text-5xl text-yellow-500 font-semibold'>
                         Affordable and quality education
                     </h1>
-                    <p className="text-xl text-gray-200">
+                    <p className="text-xl pr-5 mt-5 sm:text-xl text-gray-200">
                         Our goal is to provide the affordable and qualiy education to the world. We are providing the platform for the aspiring teachers and students to share their skills, creativity and knowledge to each other to empower and contribute in the growth and wellness of mankind. 
                     </p>
                 </section>
 
-                <div className="w-1/2">
+                <div className="hidden sm:block w-1/2">
                     <img 
                         id="test1"
                         style={{
