09a6c7b63a872d0ef0063329bc398ecaa58761c9
Aakash Pal <akashpal1482@gmail.com>
2025-02-22 09:57:59 +0530

    Integrate Vercel server creation in server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import app from "./app.js";
+import { createServer } from "@vercel/node";
 import { config } from "dotenv";
 import connectionToDB from "./db_config/db_connection.js";
 // import { v2 } from "cloudinary";
@@ -25,3 +26,5 @@ app.listen(PORT,async()=>{
     await connectionToDB();
     console.log(`App is running at http://localhost:${PORT}`);
 })
+
+export default createServer(app);
